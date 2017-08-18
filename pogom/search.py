3b1f40890ab806a2dfa6cfb9c6c42735f1a18c4a
sLoPPydrive <sloppydrive@gmail.com>
2017-08-18 15:39:20 +0200

    Fix failed account view under certain circumstances

--- pogom/search.py
+++ pogom/search.py
@@ -210,6 +210,9 @@ def status_printer(threadStatus, account_failures, logmode, hash_key,
             status_text.append('Accounts on hold:')
             status_text.append('-----------------------------------------')
 
+            # There is only one page
+            total_pages = 1
+
             # Find the longest account name.
             userlen = 4
             for account in account_failures:
